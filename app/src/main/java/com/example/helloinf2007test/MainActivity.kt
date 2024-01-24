18c6fc13a00d6895c61f4b597460d6b1b9f55a16
chang an <Changan35@gmail.com>
2024-01-24 14:02:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloinf2007test/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.helloinf2007test
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloinf2007test.ui.theme.HelloINF2007testTheme
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // get current date and time
+        val currentDateTime = LocalDateTime.now()
+        // define a custom date and time format
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        // format the current date and time using the formatter
+        val formattedDateTime = currentDateTime.format(formatter)
+        Log.d("MainActivity", "OnCreate function is created at $formattedDateTime")
+        
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloINF2007testTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        Greeting("Android")
+                        From("Michael")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun From(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "From $name",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloINF2007testTheme {
+        Greeting("Android")
+    }
+}
